0df682a8763b4695579a8620a2e7b48dc1d3179b
Moritz Höwer <Moritz.Hoewer@haw-hamburg.de>
2016-10-11 23:56:45 +0200

    Improved Performance Evaluation

--- src/adp/aufgabe4/SortingPerformanceEvaluator.java
+++ src/adp/aufgabe4/SortingPerformanceEvaluator.java
@@ -13,211 +13,207 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+import adp.util.AbstractPerformanceEvaluator;
+
 /**
  * Provides methods to evaluate the performance of bubble sort and insertionsort
  * on an ArrayList
  *
- * @author Moritz Höwer
- * @version 1.0 - 08.10.2016
+ * @author Moritz Höwer, Jesko Treffler
+ * @version 1.0 - 11.10.2016
  */
-public class SortingPerformanceEvaluator {
+public class SortingPerformanceEvaluator extends AbstractPerformanceEvaluator {
 
-	/**
-	 * the Randomizer for filling the List
-	 */
-	private Random random;
+    /**
+     * the randomizer for filling the List
+     */
+    private Random random;
 
-	/**
-	 * how often is the list sorted for evaluation
-	 */
-	private static final int NUMBER_OF_SORTS = 100;
+    /**
+     * the seed for the randomizer
+     */
+    private long seed;
 
-	/**
-	 * maximum list size to test
-	 */
-	private static final int MAX_SIZE = 10000-1;
+    /**
+     * how often is the list sorted for evaluation
+     */
+    private static final int NUMBER_OF_SORTS = 100;
 
-	/**
-	 * stores the values to generate input for matlab
-	 */
-	private HashMap<String, ArrayList<Long>> forMatlab;
+    /**
+     * maximum list size to test
+     */
+    private static final int MAX_SIZE = 10000;
 
-	public SortingPerformanceEvaluator(long seed) {
-		random = new Random(seed);
-		forMatlab = new HashMap<>();
-	}
+    public SortingPerformanceEvaluator(long seed) {
+        this.seed = seed;
+    }
 
-	/**
-	 * Fills the list with random numbers
-	 * 
-	 * @param list
-	 *            the list to fill
-	 * @param desiredSize
-	 *            how many items are to be inserted
-	 */
-	private void fillListRandomly(SortableArrayList list, int desiredSize) {
-		list.empty();
-		int i = 0;
-		int n = 0;
+    /**
+     * Fills the list with random numbers
+     * 
+     * @param list
+     *            the list to fill
+     * @param desiredSize
+     *            how many items are to be inserted
+     */
+    private void fillListRandomly(SortableArrayList list, int desiredSize) {
+        list.empty();
+        int i = 0;
+        int n = 0;
 
-		while (n < desiredSize) {
-			// decide whether current i should be added
-			if (random.nextBoolean()) {
-				int index = random.nextInt(n + 1);
-				list.insert(index, i);
-				n++;
-			}
-			i++;
-		}
+        while (n < desiredSize) {
+            // decide whether current i should be added
+            if (random.nextBoolean()) {
+                int index = random.nextInt(n + 1);
+                list.insert(index, i);
+                n++;
+            }
+            i++;
+        }
 
-	}
+    }
 
-	/**
-	 * evaluates perfomance by running {@code function} {@code NUMBER_OF_SORTS}
-	 * times on {@code list} with a list size of {@code size}.
-	 * 
-	 * @param list
-	 *            the list to perform function on
-	 * @param size
-	 *            the size of the list
-	 * @param function
-	 *            the function to perform
-	 * @return ns per operation
-	 */
-	private long[] evaluatePerformanceForSize(SortableArrayList list, int size,
-			Runnable function) {
-		
-		// counters for ns
-		long nanosAccumulator= 0;
-		long nanosMin = Long.MAX_VALUE;
-		long nanosMax = Long.MIN_VALUE;
-		long nanoStart, nanoEnd;
-		
-		for (int i = 0; i < NUMBER_OF_SORTS; i++) {
-			fillListRandomly(list, size);
-			
-			nanoStart = System.nanoTime();
-			function.run();
-			nanoEnd = System.nanoTime();
-			
-			long nanos = (nanoEnd - nanoStart);
-			nanosMin = Math.min(nanos,  nanosMin);
-			nanosMax = Math.max(nanos,  nanosMax);
-			
-			nanosAccumulator += nanos;
-		}
-		return new long[]{nanosMin, nanosMax, nanosAccumulator / NUMBER_OF_SORTS};
-	}
+    /**
+     * evaluates perfomance by running {@code function} {@code NUMBER_OF_SORTS}
+     * times on {@code list} with a list size of {@code size}.
+     * 
+     * @param list
+     *            the list to perform function on
+     * @param size
+     *            the size of the list
+     * @param function
+     *            the function to perform
+     * @return ns per operation
+     */
+    private long[] evaluatePerformanceForSize(SortableArrayList list, int size,
+            Runnable function) {
 
-	/**
-	 * Evaluates {@code function} on {@code list}
-	 * 
-	 * @param name
-	 *            the name of the function for HashMap
-	 * @param list
-	 *            the list to performs operations in
-	 * @param function
-	 *            the function to perform
-	 */
-	private void evaluateAlgorithm(String name, SortableArrayList list,
-			Runnable function) {
-		System.out.println("Evaluating " + name);
-		for (int i = 10; i <= MAX_SIZE; i *= 10) {
-			// fill List randomly once so array size is already correct
-			fillListRandomly(list, i);
+        // counters for ns
+        long nanosAccumulator = 0;
+        long nanosMin = Long.MAX_VALUE;
+        long nanosMax = Long.MIN_VALUE;
+        long nanoStart, nanoEnd;
 
-			System.out.print("Size " + i + " ...");
-			long[] ns = evaluatePerformanceForSize(list, i, function);
-			
-			// add result to matlab
-			forMatlab.get(name + "_min").add(ns[0]);
-			forMatlab.get(name + "_max").add(ns[1]);
-			forMatlab.get(name + "_avg").add(ns[2]);
-			
-			// print result in a nice form
-			System.out.format("%s | %s | %s per sorting\n", makeNice(ns[0]), makeNice(ns[2]), makeNice(ns[1]));
-		}
+        for (int i = 0; i < NUMBER_OF_SORTS; i++) {
+            fillListRandomly(list, size);
 
-		System.out.println("Done");
-	}
+            nanoStart = System.nanoTime();
+            function.run();
+            nanoEnd = System.nanoTime();
 
-	/**
-	 * performs the evaluation
-	 */
-	private void performEvaluation() {
-		// init HashMap
-		forMatlab.put("sizes", new ArrayList<>());
-		
-		forMatlab.put("BS_min", new ArrayList<>());
-		forMatlab.put("BS_max", new ArrayList<>());
-		forMatlab.put("BS_avg", new ArrayList<>());
-		
-		forMatlab.put("IS_min", new ArrayList<>());
-		forMatlab.put("IS_max", new ArrayList<>());
-		forMatlab.put("IS_avg", new ArrayList<>());
-		
-		for (int i = 10; i <= MAX_SIZE; i *= 10) {
-			forMatlab.get("sizes").add(new Long(i));
-		}
-		
-		SortableArrayList list = new SortableArrayList();
+            long nanos = (nanoEnd - nanoStart);
+            nanosMin = Math.min(nanos, nanosMin);
+            nanosMax = Math.max(nanos, nanosMax);
 
-		evaluateAlgorithm("BS", list, list::doBubbleSort);
-		evaluateAlgorithm("BS", list, list::doBubbleSortfaster);
-		evaluateAlgorithm("BS", list, list::doBubbleSortEvenFaster);
-		evaluateAlgorithm("IS", list, list::doInsertionSort);
-		
-		// output matlab
-		forMatlab.forEach((s, a) -> {
-			System.out.print(s + " = [");
-			for(int i = 0; i < a.size(); i++){
-				System.out.print(a.get(i));
-				if( i != a.size() - 1){
-					System.out.print(", ");
-				}
-			}
-			System.out.println("];");
-		}); 
-	}
+            nanosAccumulator += nanos;
+        }
+        return new long[]{nanosMin, nanosMax,
+                nanosAccumulator / NUMBER_OF_SORTS};
+    }
 
-	/**
-	 * Converts the nanoseconds to the most appropriate unit
-	 * 
-	 * @param nanos
-	 *            number of nanoseconds
-	 * @return String representation in ns, µs or ms
-	 */
-	private String makeNice(long nanos) {
-		if (nanos < 1000) {
-			return nanos + " ns";
-		} else {
-			double us = nanos / 1000.0;
-			if (us < 1000) {
-				return String.format("%.2f µs", us);
-			} else {
-				double ms = us / 1000;
-				return String.format("%.2f ms", ms);
-			}
-		}
-	}
+    /**
+     * Evaluates {@code function} on {@code list}
+     * 
+     * @param name
+     *            the name of the function for HashMap
+     * @param list
+     *            the list to performs operations in
+     * @param function
+     *            the function to perform
+     */
+    private void evaluateAlgorithm(String name, SortableArrayList list,
+            Runnable function) {
+        System.out.println("Evaluating " + name);
+        for (int i = 1000; i <= MAX_SIZE; i += 1000) {
+            // fill List randomly once so array size is already correct
+            fillListRandomly(list, i);
 
-	/**
-	 * Program entry point
-	 * 
-	 * @param args
-	 *            comand line args
-	 */
-	public static void main(String[] args) {
-		SortingPerformanceEvaluator eval = new SortingPerformanceEvaluator(
-				"123Kartoffelkotze".hashCode());
-		
-		eval.performEvaluation();
-	/*	SortableArrayList list = new SortableArrayList();
-		eval.fillListRandomly(list, 10);
-		System.out.println(list);
-		list.doBubbleSortEvenFaster();
-		System.out.println(list);
-*/
-	}
+            System.out.print("Size " + i + " ...");
+            long[] ns = evaluatePerformanceForSize(list, i, function);
+
+            // add result to matlab
+            addValueToMatlab(name, "MIN", ns[0]);
+            addValueToMatlab(name, "AVG", ns[2]);
+            addValueToMatlab(name, "MAX", ns[1]);
+
+            // print result in a nice form
+            System.out.format("%s | %s | %s per sorting\n", makeNice(ns[0]),
+                    makeNice(ns[2]), makeNice(ns[1]));
+        }
+
+        System.out.println("Done");
+    }
+
+    /*
+     * performs the evaluation
+     */
+    @Override
+    public void performEvaluation() {
+        // init Matlab output
+        initMatlab(new String[]{"BS", "BS_better", "BS_best", "IS"},
+                new String[]{"MIN", "AVG", "MAX"});
+        addKeyToMatlab("sizes");
+
+   
+        for (int i = 1000; i <= MAX_SIZE; i += 1000) {
+            addValueToMatlab("sizes", i);
+        }
+
+        SortableArrayList list = new SortableArrayList();
+
+        random = new Random(seed);
+        evaluateAlgorithm("BS", list, list::doBubbleSort);
+
+        random = new Random(seed);
+        evaluateAlgorithm("BS_better", list, list::doBubbleSortfaster);
+
+        random = new Random(seed);
+        evaluateAlgorithm("BS_best", list, list::doBubbleSortEvenFaster);
+
+        random = new Random(seed);
+        evaluateAlgorithm("IS", list, list::doInsertionSort);
+
+        // output matlab
+        printMatlab();
+    }
+
+    /**
+     * Converts the nanoseconds to the most appropriate unit
+     * 
+     * @param nanos
+     *            number of nanoseconds
+     * @return String representation in ns, µs or ms
+     */
+    private String makeNice(long nanos) {
+        if (nanos < 1000) {
+            return nanos + " ns";
+        } else {
+            double us = nanos / 1000.0;
+            if (us < 1000) {
+                return String.format("%.2f µs", us);
+            } else {
+                double ms = us / 1000;
+                return String.format("%.2f ms", ms);
+            }
+        }
+    }
+
+    /**
+     * Program entry point
+     * 
+     * @param args
+     *            comand line args
+     */
+    public static void main(String[] args) {
+        SortingPerformanceEvaluator eval = new SortingPerformanceEvaluator(
+                "123Kartoffelkotze".hashCode());
+
+        eval.performEvaluation();
+        /*
+         * SortableArrayList list = new SortableArrayList();
+         * eval.fillListRandomly(list, 10); System.out.println(list);
+         * list.doBubbleSortEvenFaster(); System.out.println(list);
+         */
+    }
 
 }
---
+++ src/adp/util/AbstractPerformanceEvaluator.java
@@ -0,0 +1,107 @@
+/*
+ * Praktikum AD - WS 2016
+ * Gruppe:  Jesko Treffler (Jesko.Treffler@haw-hamburg.de),
+ * 			Moritz Höwer (Moritz.Hoewer@haw-hamburg.de)
+ * 
+ * Datum: 11.10.2016 
+ * Aufgabe: Aufgabenblatt X
+ */
+
+package adp.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Abstract superclass for performance evaluators
+ *
+ * @author Moritz Höwer
+ * @version 1.0 - 11.10.2016
+ */
+public abstract class AbstractPerformanceEvaluator {
+    /**
+     * stores the values to generate input for matlab
+     */
+    private HashMap<String, ArrayList<Long>> forMatlab;
+
+    protected AbstractPerformanceEvaluator() {
+        forMatlab = new HashMap<>();
+    }
+
+    /**
+     * Initializes matlab output
+     * 
+     * @param keys the primary keys
+     * @param subkeys the subkeys
+     */
+    protected void initMatlab(String[] keys, String[] subkeys) {
+        for(String key : keys){
+            for(String subkey : subkeys){
+                addKeyToMatlab(key + "_" + subkey);
+            }
+        }
+    }
+    
+    /**
+     * Initializes matlab output
+     * 
+     * @param keys the primary keys
+     */
+    protected void initMatlab(String... keys) {
+        for(String key : keys){
+            addKeyToMatlab(key);
+        }
+    }
+    
+    
+    /**
+     * Adds a key to the matlab output
+     * 
+     * @param key the key to add
+     */
+    protected void addKeyToMatlab(String key){
+        forMatlab.put(key, new ArrayList<>());
+    }
+    
+    /**
+     * Adds a value to the matlab output
+     * 
+     * @param key the key for which the value is to be added
+     * @param subkey the subkey for which to add
+     * @param value the value to be added
+     */
+    protected void addValueToMatlab(String key, String subkey, long value) {
+        addValueToMatlab(key + "_" + subkey, value);
+    }
+
+    /**
+     * Adds a value to the matlab output
+     * 
+     * @param key the key for which the value is to be added
+     * @param value the value to be added
+     */
+    protected void addValueToMatlab(String key, long value) {
+        forMatlab.get(key).add(value);
+    }
+
+    /**
+     * Prints matlab output
+     */
+    protected void printMatlab() {
+        forMatlab.forEach((s, a) -> {
+            System.out.print(s + " = [");
+            for (int i = 0; i < a.size(); i++) {
+                System.out.print(a.get(i));
+                if (i != a.size() - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("];");
+        });
+    }
+    
+    /**
+     * Perform evaluation of Tasks
+     */
+    public abstract void performEvaluation();
+}
