08d9643091c91ca8017a7dc3b18fc9f8e67a2d06
Moritz Hoewer <Moritz.Hoewer@haw-hamburg.de>
2016-10-26 13:58:51 +0200

    Fully implemented huffman encoding

--- src/adp/aufgabe6/HuffmanApplication.java
+++ src/adp/aufgabe6/HuffmanApplication.java
@@ -9,13 +9,16 @@
 
 package adp.aufgabe6;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * The application
  *
  * @author Moritz Höwer
- * @version 1.0 - 23.10.2016
+ * @version 1.0 - 26.10.2016
  */
 public class HuffmanApplication {
 
@@ -23,32 +26,69 @@ public class HuffmanApplication {
      * @param args
      */
     public static void main(String[] args) {
-        final String TEXT = "MISSISIPPI";
+        // read reftext (introduction to sherlock holmes ~30k chars)
+        String ref = readFile("reftext.txt");
 
-        // missing analysis => putting dummy code
-        ArrayList<HuffmanSingleCharTuple> occurrences = new ArrayList<>();
-        occurrences.add(new HuffmanSingleCharTuple(1, 'M'));
-        occurrences.add(new HuffmanSingleCharTuple(2, 'P'));
-        occurrences.add(new HuffmanSingleCharTuple(4, 'I'));
-        occurrences.add(new HuffmanSingleCharTuple(4, 'S'));
+        // a text for encoding and decoding
+        final String TEXT = "If you give someone a program you will frustrate "
+                + "them for a day. If you teach them how to program, you will "
+                + "frustrate them for a lifetime. The computing scientist's "
+                + "main challenge is not to get confused by the complexities "
+                + "of his own making. Beauty is more important in computing "
+                + "than anywhere else in technology because software is so "
+                + "complicated. Beauty is the ultimate defence against "
+                + "complexity.";
 
-        // convert occurrences List to HuffmanTree (with the encoding)
-        // @Jesko - wärste mal zu der Streams Vorlesung von Jenke gekommen ;)
-        // die sind nämlich ziemlich aweseome
-        HuffmanTree encodingTree = occurrences.stream()
-                .sorted() // sort by # of occurrences
-                .map(HuffmanTree::new) // convert to trees
-                .reduce(new HuffmanTree(), HuffmanTree::combineWith);
+        HuffmanEncoder encoder = new HuffmanEncoder();
+        // generate tree with reftext
+        encoder.generateHuffmanTree(ref);
 
-        // DEBUG: print out encoding table
-        System.out.println(encodingTree);
-        TEXT.chars().distinct().forEach(i -> {
-            char c = (char) i;
-            System.out.println(c + " -> " + encodingTree.getHuffmanCodeFor(c));
-        });
+        String code = encoder.encode(TEXT);
+        System.out.println("Code is " + code.length() + " Bit instead of "
+                + TEXT.length() * 8 + " Bit when encoded with REFTEXT");
+        System.out.println(code);
+        System.out.println(encoder.decode(code));
 
-        // missing: print encoded Text
+        System.out.println();
+        System.out.println(
+                "------------------------------------------------------------");
+        System.out.println();
 
+        // generate tree with text itself
+        encoder.generateHuffmanTree(TEXT);
+
+        code = encoder.encode(TEXT);
+        System.out.println("Code is " + code.length() + " Bit instead of "
+                + TEXT.length() * 8 + " Bit when encoded with itself");
+        System.out.println(code);
+        System.out.println(encoder.decode(code));
+    }
+
+    /**
+     * Reads the entire file specified under filename to a String and returns it
+     * 
+     * @param filename
+     *            the (relative) filename to read
+     * @return the contents of the file as a String
+     */
+    private static String readFile(String filename) {
+        // get the File as an InputStream
+        InputStream is = HuffmanApplication.class.getResourceAsStream(filename);
+
+        // read it
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is))) {
+            StringBuilder builder = new StringBuilder();
+
+            String s;
+            while ((s = reader.readLine()) != null) {
+                builder.append(s);
+            }
+
+            return builder.toString();
+        } catch (IOException e) {
+            return "";
+        }
     }
 
 }
--- src/adp/aufgabe6/HuffmanTree.java
+++ src/adp/aufgabe6/HuffmanTree.java
@@ -15,7 +15,7 @@ import java.util.HashSet;
  * A BinaryTree for the Huffman code
  *
  * @author Moritz Höwer
- * @version 1.0 - 22.10.2016
+ * @version 1.0 - 26.10.2016
  */
 public class HuffmanTree {
     /**
@@ -49,54 +49,58 @@ public class HuffmanTree {
     }
 
     /**
-     * returns a new Huffman Tree that contains this and other as childs
+     * returns a new HuffmanTree that contains {@code this} and {@code other} as
+     * childs
      * 
-     * @param other the tree to combine this with
-     * @return a new Tree containing this and other
+     * @param other
+     *            the tree to combine {@code this} with
+     * @return a new Tree containing {@code this} and {@code other}
      */
-    public HuffmanTree combineWith(HuffmanTree other){
+    public HuffmanTree combineWith(HuffmanTree other) {
         // for starting reduce operation of stream
-        if(value == null){
+        if (value == null) {
             return other;
         }
-        
+
         HuffmanTree left, right;
-        if(this.value.compareTo(other.value) < 0){
+        if (this.value.compareTo(other.value) < 0) {
             left = this;
             right = other;
         } else {
             left = other;
             right = this;
         }
-        
+
         // create parameters for new Tuple
         int newTimes = this.value.getTimes() + other.value.getTimes();
         HashSet<Character> newSet = new HashSet<>(this.value.getCharacters());
         newSet.addAll(other.value.getCharacters());
 
         // create new tuple
-        HuffmanMultiCharTuple newTuple = new HuffmanMultiCharTuple(newTimes, newSet);
-        
+        HuffmanMultiCharTuple newTuple = new HuffmanMultiCharTuple(newTimes,
+                newSet);
+
         // create and return new tree
         return new HuffmanTree(newTuple, left, right);
     }
-    
+
     /**
      * Generates the Huffman code for c from the tree
      * 
-     * @param c the character to encode
+     * @param c
+     *            the character to encode
      * @return the Huffman code for c
      */
-    public String getHuffmanCodeFor(char c){
-        if(leftChild.value.hasCharacter(c)){
-            if(leftChild.isLeaf()){
+    public String getHuffmanCodeFor(char c) {
+        if (leftChild.value.hasCharacter(c)) {
+            if (leftChild.isLeaf()) {
                 // we're at the end ==> quit recursion
                 return "0";
             } else {
                 return "0" + leftChild.getHuffmanCodeFor(c);
             }
-        } else if(rightChild.value.hasCharacter(c)) {
-            if(rightChild.isLeaf()){
+        } else if (rightChild.value.hasCharacter(c)) {
+            if (rightChild.isLeaf()) {
                 // we're at the end ==> quit recursion
                 return "1";
             } else {
@@ -104,14 +108,52 @@ public class HuffmanTree {
             }
         } else {
             // character not found
-            throw new IllegalArgumentException("Can't encode '" + c + "' because it is not in the Tree.");
+            throw new IllegalArgumentException(
+                    "Can't encode '" + c + "' because it is not in the Tree.");
         }
     }
-    
+
+    /**
+     * decodes Huffman code into chars.
+     * 
+     * @param code
+     *            the Huffman code to decode <b>WARNING:</b> will be modified!
+     * @return the next character from the code sequence <b>SIDE EFFECT:</b> all
+     *         processed Bits will be removed from {@code code}
+     */
+    public char getCharForHuffmanCode(StringBuilder code) {
+        HuffmanTree node = this;
+        // traverse tree according to code
+        while (!node.isLeaf()) {
+            if (code.length() == 0) {
+                throw new IllegalArgumentException("Code is invalid");
+            }
+            if (code.charAt(0) == '0') {
+                // go to left
+                node = node.leftChild;
+            } else if (code.charAt(0) == '1') {
+                // go to right
+                node = node.rightChild;
+            } else {
+                throw new IllegalArgumentException(
+                        "Code is invalid - character '" + code.charAt(0)
+                                + "' not allowed!");
+            }
+            // remove first char
+            code.deleteCharAt(0);
+        }
+        if (node.value == null) {
+            throw new IllegalStateException("Tree is not valid!");
+        }
+
+        // retrieve the character
+        return ((HuffmanSingleCharTuple) node.value).getCharacter();
+    }
+
     /**
      * whether this tree/node is at the edge of the tree
      * 
-     * @return true if it's a leaf node
+     * @return {@code true} if it's a leaf node
      */
     public boolean isLeaf() {
         return leftChild == null || rightChild == null;
